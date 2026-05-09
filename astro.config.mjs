b183647d1ad6dcafb2f67709d9c8a96031d1c603
Doug Hoyer <doughoyer@exposure.systems>
2026-05-09 07:49:52 -0700

    Clean template naming

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jamstack-template-1ic.pages.dev', // ?? update later if you use a custom domain
+  site: 'https://pengosystems.com', // ?? update later if you use a custom domain
   vite: {
     plugins: [tailwindcss()]
   },
