6e9fdac9f3ef4ffcf25af366ba1b15651ddb76a4
Doug Hoyer <doughoyer@exposure.systems>
2026-05-09 18:17:08 -0700

    Add Workbench machine sitemap

---
+++ src/pages/workbench/machine-sitemap.xml.ts
@@ -0,0 +1,39 @@
+import ontologyIndex from "../../content/_meta/ontology-index.json";
+
+function xmlEscape(value: string) {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+export function GET() {
+  const baseUrl = "https://pengosystems.com";
+
+  const urls = [
+    "/workbench/llms.txt",
+    ...ontologyIndex.issues.map((issue) => issue.markdown_url),
+    ...ontologyIndex.patterns.map((pattern) => pattern.markdown_url),
+    ...ontologyIndex.lenses.map((lens) => lens.markdown_url),
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${xmlEscape(`${baseUrl}${url}`)}</loc>
+  </url>`
+  )
+  .join("\n")}
+</urlset>
+`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+}
